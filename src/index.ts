656c7c3c122cf76accae8cf60870047caec36dec
adictya <aditya.26101@gmail.com>
2022-11-28 16:06:03 +0530

    backup

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import AgoraRTC, { EncryptionMode } from "agora-rtc-sdk-ng";
+import test from "./testFile";
 
 export default class SuperSdk {
   encryptionMode: EncryptionMode = "aes-128-xts";
   createClient() {
     AgoraRTC.createClient({ mode: "rtc", codec: "vp9" });
   }
+  testFunc = test;
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
+import fs from "fs";
 import { resolve } from "path";
+
+import packageJson from "./package.json";
+
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 import { externalizeDeps } from "vite-plugin-externalize-deps";
+
+Object.keys(packageJson.dependencies).map((v) => {
+  console.log(`node_modules/${v}/**`);
+});
 
 export default defineConfig({
   build: {
@@ -18,6 +26,31 @@ export default defineConfig({
       staticImport: true,
       insertTypesEntry: true,
       rollupTypes: true,
+      // include: ["./src/**", "./node_modules/agora-rtc-sdk-ng/rtc-sdk_en.d.ts"],
+      // exclude: [],
+      // include: [...Object.keys(packageJson.dependencies).map(
+      //   (v) => `node_modules/${v}/**`
+      // ),'./src/index.ts'],
+      // afterBuild: () => {
+      //   fs.readFile("dist/super-sdk.d.ts", "utf8", function (err, data) {
+      //     if (err) {
+      //       return console.log(err);
+      //     }
+      //     var result = data.replace(
+      //       /agora-rtc-sdk-ng/g,
+      //       "./lib/agora-rtc-sdk-ng"
+      //     );
+      //
+      //     fs.writeFile("dist/super-sdk.d.ts", result, "utf8", function (err) {
+      //       if (err) return console.log(err);
+      //     });
+      //   });
+      //   fs.cp(
+      //     "./node_modules/agora-rtc-sdk-ng/rtc-sdk_en.d.ts",
+      //     "./dist/lib/agora-rtc-sdk-ng.d.ts",
+      //     () => {}
+      //   );
+      // },
     }),
   ],
 });
